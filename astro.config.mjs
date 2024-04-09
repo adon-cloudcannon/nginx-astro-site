a34b58b68b61fa613a267329619167059fe043c5
Adon Moskal <adonmoskal@Adons-MBP.cloudcannon.office>
2024-04-10 08:24:09 +1200

    Feat: adding the component for NXT_Hint and more config options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import AutoImport from 'astro-auto-import';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [
+        AutoImport({
+            imports: [
+            '@components/NXT_Hint.astro',
+            ],
+        }),
+        mdx()
+    ],
 
     markdown: {
         syntaxHighlight: 'prism',
